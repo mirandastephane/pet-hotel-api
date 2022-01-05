5d12b28df0fbe551ba3afee54a29349862de3656
mirandastephane <duarte.stephane@gmail.com>
2022-01-04 22:49:18 -0300

    New Endpoints and SwaggerConfig

--- src/main/java/com/pet/hotel/controller/TutorController.java
+++ src/main/java/com/pet/hotel/controller/TutorController.java
@@ -4,30 +4,72 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pet.hotel.exception.ResourceNotFoundException;
 import com.pet.hotel.model.Tutor;
 import com.pet.hotel.repository.TutorRepository;
 
 @RestController
-@RequestMapping("/tutor")
+@RequestMapping("/api")
 public class TutorController {
 	
 	@Autowired
 	private TutorRepository tutorRepository;
 
-	@GetMapping
-	public List<Tutor> listar() {
+	// Get All Tutors
+	@GetMapping("/tutor")
+	public List<Tutor> getAllTutor() {
 		return tutorRepository.findAll();
 	}
-	@PostMapping
+	
+	// Create a new Tutor
+	@PostMapping("/tutor")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Tutor adicionar(@RequestBody Tutor tutor) {
-		return tutorRepository.save(tutor);
+	public Tutor createTutor(@RequestBody Tutor tutor) {
+		return (Tutor) tutorRepository.save(tutor);
 	}
+	
+	// Get a Single Tutor
+	@GetMapping("/tutor/{id}")
+	public Tutor getTutorById(@PathVariable(value = "id") Long tutorId) throws Throwable {
+		return (Tutor) tutorRepository.findById(tutorId)
+				.orElseThrow(() -> new ResourceNotFoundException ("Tutor", "id", tutorId));
+	}
+	
+	// Update a Tutor
+    @PutMapping("/tutor/{id}")
+    public Tutor updateReservation(@PathVariable(value = "id") Long tutorId,
+    		@RequestBody Tutor tutorDetails) throws Throwable {
+
+        Tutor tutor = (Tutor) tutorRepository.findById(tutorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Tutor", "id", tutorId));
+
+        tutor.setNome(tutorDetails.getNome());
+        tutor.setDataNascimento(tutorDetails.getDataNascimento());
+        tutor.setCpf(tutorDetails.getCpf());
+
+        Tutor updatedTutor = (Tutor) tutorRepository.save(tutor);
+        return updatedTutor;
+    }
+    
+    // Delete a Tutor
+    @DeleteMapping("/tutor/{id}")
+    public ResponseEntity<?> deleteTutor(@PathVariable(value = "id") Long tutorId) throws Throwable {
+    	Tutor tutor = (Tutor) tutorRepository.findById(tutorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", tutorId));
+
+        tutorRepository.delete(tutor);
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/pet/hotel/model/Tutor.java
+++ src/main/java/com/pet/hotel/model/Tutor.java
@@ -2,21 +2,36 @@ package com.pet.hotel.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
 public class Tutor {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false)
-	private String nome;	
+	@Column
+	private String nome;
+	
+	@Column
+	private String dataNascimento;
+	
+	@Column
+	private String cpf;
+	
 	
 }
